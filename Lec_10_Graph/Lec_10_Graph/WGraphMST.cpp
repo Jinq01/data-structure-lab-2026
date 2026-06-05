7b797e5fc50cea04adf30613db26491cf28538d7
kjg <rnjswlsrb46120@jbnu.ac.kr>
2026-06-05 10:34:23 +0900

    Prim, Dijkstra 알고리즘 추가

--- Lec_10_Graph/Lec_10_Graph/WGraphMST.cpp
+++ Lec_10_Graph/Lec_10_Graph/WGraphMST.cpp
@@ -1,8 +1,25 @@
 #include "WGraphMST.h"
+#include "WGraphPrim.h"
+#include "WGraphDijkstra.h"
+
 int main() {
-	WGraphMST g;
+	/*WGraphMST g;
 	g.load("graphs.txt");
 	printf("MST By Krukal`s Algorithm\n");
 	g.Kruskal();
+	return 0;*/
+
+	WGraphPrim g;
+	g.load("graph.txt");
+
+	printf("MST By Prim'Algorithm\n");
+	g.Prim(0);
+	
+
+	WGraphdijkstra g1;
+	g1.load("graph_sp.txt");
+	printf("Shortest Path By Dijkstra Algorithm\n");
+	g1.ShortestPath(0);
+
 	return 0;
 }
